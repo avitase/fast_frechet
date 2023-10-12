bab56ebab15dc364a37d143a47ccdd61cf1c4f53
Nis Meinert <nis.meinert@dlr.de>
2023-10-12 16:59:13 +0200

    Improve code

--- fast_frechet/levenshtein.py
+++ fast_frechet/levenshtein.py
@@ -17,9 +17,9 @@ def levenshtein_distance(p, q):
 
     v = np.arange(Q + 1)
     for i in range(P):
-        d = np.where(p[i] == q, v[:-1], np.inf)
+        d = np.where(p[i] == q, 0, np.inf)
         u = np.minimum(v[:-1], v[1:])
 
-        v = list(accumulate(zip(u, d), levenshtein_min, initial=i + 1))
+        v = list(accumulate(zip(u, d + v[:-1]), levenshtein_min, initial=i + 1))
 
     return v[-1]
