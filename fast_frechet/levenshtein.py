a101522efc680c694dc641c3cc2adb45cf01dfca
Nis Meinert <mail@nismeinert.de>
2023-10-13 12:54:57 +0200

    Improve code

--- fast_frechet/levenshtein.py
+++ fast_frechet/levenshtein.py
@@ -3,6 +3,10 @@ from itertools import accumulate
 import numpy as np
 
 
+def metric(a, b):
+    return np.where(a == b, 0, np.inf)
+
+
 def levenshtein_min(acc, x):
     u, d = x
     return min(1 + acc, 1 + u, d)
@@ -15,9 +19,11 @@ def levenshtein_distance(p, q):
     P = len(p)
     Q = len(q)
 
-    v = np.arange(Q + 1)
-    for i in range(P):
-        d = np.where(p[i] == q, 0, np.inf)
+    v = np.minimum(1, metric(p[0], q)) + np.arange(Q)
+    v = np.insert(v, 0, 1)
+
+    for i in range(1, P):
+        d = metric(p[i], q)
         u = np.minimum(v[:-1], v[1:])
 
         v = list(accumulate(zip(u, d + v[:-1]), levenshtein_min, initial=i + 1))
