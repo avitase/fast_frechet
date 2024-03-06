850ae0a568c49ce1318eb61153122d2c4ad5d23a
Nis Meinert <mail@nismeinert.de>
2024-03-06 18:06:23 +0100

    Enforce black compatible formatting

--- viz/conf.py
+++ viz/conf.py
@@ -6,29 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'fast_frechet'
-copyright = '2024, Nis Meinert'
-author = 'Nis Meinert'
-release = '0.1.0'
+project = "fast_frechet"
+copyright = "2024, Nis Meinert"
+author = "Nis Meinert"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'nbsphinx',
+    "nbsphinx",
 ]
 
 templates_path = []
-exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
-
+exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['static']
+html_theme = "alabaster"
+html_static_path = ["static"]
 
 html_theme_options = {
-    'nosidebar': True,
+    "nosidebar": True,
 }
